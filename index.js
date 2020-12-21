6cc9068355b49415bf892c9dcd9238e9017746a1
Lucas Falaschini <lucasfalaschini@gmail.com>
2020-12-21 20:49:36 -0300

    SPA control

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const path = require('path');
 
 const express = require('express');
 const cors = require('cors');
@@ -34,7 +35,10 @@ app.use('/api/medicos', require('./routes/medicos'));
 app.use('/api/todo', require('./routes/busquedas'));
 app.use('/api/upload', require('./routes/uploads'));
 
-
+// Lo último
+app.get('*', (req, res) => {
+    res.sendFile( path.resolve( __dirname, 'public/index.html'));
+});
 
 
 
